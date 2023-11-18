010f3f6ce45c9a09e89b7ced3c0a173c5d4425dc
Archit Kandu <architkandu@hotmail.com>
2023-11-18 21:19:59 +0530

    Registration API Done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const dotenv = require("dotenv");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 app.use(express.json());
@@ -11,5 +13,10 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("API is Called!");
 });
+
+app.use("/api/user", userRoutes);
+// app.use("/api/medicine", medicineRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 const server = app.listen(5000, console.log("Server Started on PORT 5000!"));
